431221a5338287f5fba0ee1732abe524a055bd33
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2022-09-08 14:31:59 -0600

    stashing work

--- scripts/profiles.sh
+++ scripts/profiles.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
 ###############################################################################
 # Update Server Profiles
@@ -21,73 +21,71 @@
 
 # Exit immediately if non zero code
 set -e
-CWD=$(dirname "$0")
 # Source global functions and variables
+CWD=$(dirname "$0")
 . "${CWD}/vars.sh"
 . "${CWD}/functions.sh"
 getLocalSecrets
 getEnv
 
+
 # Make sure script is executed from correct directory
-this_dir=${PWD##*/}  
+this_dir=${PWD##*/}
+project=${this_dir}
 if ! ls .git > /dev/null ; then
   echo "${RED}ERROR: This script should run from the root of your local repo.${NC}"
   exit;
 fi
 
+# Get repo name
+repo=${GITHUB_REPOSITORY}
+
 # Set project name, i.e. bxfinance or bxhealth etc
-# TODO: remove if nuances are avoided. 
-# saving for now. 
 # IFS=- read project the_rest <<< "$this_dir"
 # project=$( echo "$project" | tr -s  '[:upper:]'  '[:lower:]' )
 
 # Confirm the correct namespace
-# printf "\n\nChoose your namespace:\n"
-# select ns in $mynamespace "quit"; do
-#     case $ns in
-#         $mynamespace ) break;;
-#         quit ) printf "\n\ngood-bye!\n\n"; exit;;
-#     esac
-# done
+printf "\n\nChoose your namespace:\n"
+select ns in $K8S_NAMESPACE "quit"; do
+    case $ns in
+        $K8S_NAMESPACE ) break;;
+        quit ) printf "\n\ngood-bye!\n\n"; exit;;
+    esac
+done
 
+options=($(ls profiles))
 
-
-
-# exit 1
 # Server profile options
+# if [ "$project" = "bxhealth"  ] || [ "$project" = "bxfinance"  ]
+# then
+#   options=("PingAuthorize" "AZ Policy Administration Point (PAP)" "PingDirectory" "PingAccess" "PingFederate" "Select All")
 # else
 #   options=("PingAuthorize" "AZ Policy Administration Point (PAP)" "Select All")
 # fi
 
 # Build menu of server profiles
-_options="PingAuthorize PingAuthorizePAP PingDirectory PingAccess PingFederate SelectAll"
 menu() {
     printf "\n\nWhich server profiles do you want to update?\n"
-    _counter=0
-    for i in $_options; do 
-        printf "%3d%s) %s\n" "$((_counter + 1))" "${choices[i]:- }" "${i}"
-        _counter=$((_counter+1));
+    for i in ${!options[@]}; do 
+        printf "%3d%s) %s\n" $((i+1)) "${choices[i]:- }" "${options[i]}"
     done
 }
+
 prompt="Check an option (again to uncheck, ENTER when done): "
-while menu && read -rp "$prompt" && test -n "$REPLY" ; do
+while menu && read -rp "$prompt" num && [[ "$num" ]]; do
     # Was it a valid selection?
-    set -x
-    num=${REPLY}
-    test "$num" = '^[0-9]+$' && \
-      test "${num}" -lt 0 && \
-      test "${num}" -le ${#_options[@]} || { printf "\nInvalid selection."; continue; }
+    [[ "$num" != *[![:digit:]]* ]] && (( num > 0 && num <= ${#options[@]} )) || { printf "\nInvalid selection."; continue; }
     # Did user select all products?
-    if test "${num}" -eq ${#_options[@]}
+    if [[ $num = ${#options[@]} ]]
       then
-        for i in ${_options}; do 
+        for i in ${!options[@]}; do 
           choices[i]="+"
         done
         break;
     fi
     # Toggle selection 
-    "${num}"--
-    test -n "${choices[num]}" && choices[num]="" || choices[num]="+"
+    ((num--))
+    [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
 done
 
 printf "\n\nYou selected:\n\n"
@@ -116,46 +114,40 @@ done
 
 ping_directory() {
   printf "\n\033[0;32mStarting PingDirectory Profile Update\033[0m\n"
-
-  if [ "$project" = "bxhealth"  ]
-  then
-    pd_pod=$(kubectl get pods --selector app.kubernetes.io/name=pingdirectory | awk '/ping/ && $1 {print $1}')
-  else
-    pd_pod=$(kubectl get pods --selector role=pingdirectory | awk '/ping/ && $1 {print $1}')
-  fi
+  pd_pod=$(kubectl get pods --selector app.kubernetes.io/name=pingdirectory | awk '/ping/ && $1 {print $1}' | grep -e "-0$")
 
   printf "\n\033[0;32mRemove any previous files or folders\033[0m\n"
-  rm -rf ~/Downloads/${project}-pd-profile ~/Downloads/${project}-pd-users
-  rm -f ~/Downloads/${project}-pd-profile.tar ~/Downloads/${project}-users.tar
+  rm -rf /tmp/${project}-pd-profile /tmp/${project}-pd-users
+  rm -f /tmp/${project}-pd-profile.tar /tmp/${project}-users.tar
 
   printf "\n\033[0;32mCreate local folders\033[0m\n"
-  mkdir ~/Downloads/${project}-pd-profile
-  mkdir ~/Downloads/${project}-pd-users
+  mkdir -p /tmp/${project}-pd-profile
+  mkdir -p /tmp/${project}-pd-users
 
   printf "\n\033[0;32mGenerate PingDirectory profile and users.ldif\033[0m\n"
   kubectl exec $pd_pod -- rm -fr /opt/out/instance/profile/
   kubectl exec $pd_pod -- /opt/out/instance/bin/manage-profile generate-profile --profileRoot /opt/out/instance/profile/
   kubectl exec $pd_pod -- tar cvf /opt/out/instance/profile/pd-profile.tar /opt/out/instance/profile/
-  kubectl cp $mynamespace/$pd_pod:/opt/out/instance/profile/pd-profile.tar ~/Downloads/${project}-pd-profile.tar
+  kubectl cp $K8S_NAMESPACE/$pd_pod:/opt/out/instance/profile/pd-profile.tar /tmp/${project}-pd-profile.tar
   kubectl exec $pd_pod -- /opt/out/instance/bin/export-ldif --backendID userRoot --ldifFile /opt/out/instance/profile/${project}-users.ldif --doNotEncrypt
   kubectl exec $pd_pod -- tar cvf /opt/out/instance/profile/${project}-users.tar /opt/out/instance/profile/${project}-users.ldif
-  kubectl cp $mynamespace/$pd_pod:/opt/out/instance/profile/${project}-users.tar  ~/Downloads/${project}-users.tar
+  kubectl cp $K8S_NAMESPACE/$pd_pod:/opt/out/instance/profile/${project}-users.tar  /tmp/${project}-users.tar
 
   printf "\n\033[0;32mExtract files\033[0m\n"
-  tar -xf ~/Downloads/${project}-pd-profile.tar -C ~/Downloads/${project}-pd-profile
-  tar -xf ~/Downloads/${project}-users.tar -C ~/Downloads/${project}-pd-users
+  tar -xf /tmp/${project}-pd-profile.tar -C /tmp/${project}-pd-profile
+  tar -xf /tmp/${project}-users.tar -C /tmp/${project}-pd-users
 
   printf "\n\033[0;32mRemove setup-arguments.txt and license file(s)\033[0m\n"
-  rm -fv ~/Downloads/${project}-pd-profile/opt/out/instance/profile/setup-arguments.txt
-  rm -fv ~/Downloads/${project}-pd-profile/opt/out/instance/profile/server-root/pre-setup/*.lic
+  rm -fv /tmp/${project}-pd-profile/opt/out/instance/profile/setup-arguments.txt
+  rm -fv /tmp/${project}-pd-profile/opt/out/instance/profile/server-root/pre-setup/*.lic
 
   # Remove existing profile files from target.
   printf "\n\033[0;32mRemove existing profile files from target.\033[0m\n"
   rm -fr ./profiles/pingdirectory/pd.profile/
 
   printf "\n\033[0;32mCopy profile and users.ldif\033[0m\n"
-  cp -R ~/Downloads/${project}-pd-profile/opt/out/instance/profile/. ./profiles/pingdirectory/pd.profile/
-  cp -v ~/Downloads/${project}-pd-users/opt/out/instance/profile/${project}-users.ldif ./profiles/pingdirectory/pd.profile/ldif/userRoot
+  cp -R /tmp/${project}-pd-profile/opt/out/instance/profile/. ./profiles/pingdirectory/pd.profile/
+  cp -v /tmp/${project}-pd-users/opt/out/instance/profile/${project}-users.ldif ./profiles/pingdirectory/pd.profile/ldif/userRoot
   
   printf "\n\033[0;32mPingDirectory complete!\033[0m\n"
 }
@@ -173,32 +165,32 @@ ping_authorize() {
   fi
 
   printf "\n\033[0;32mRemove any previous files or folders\033[0m\n"
-  rm -rf ~/Downloads/${project}-paz-profile
-  rm -f ~/Downloads/${project}-paz-profile.tar
+  rm -rf /tmp/${project}-paz-profile
+  rm -f /tmp/${project}-paz-profile.tar
 
   printf "\n\033[0;32mCreate local folders\033[0m\n"
-  mkdir ~/Downloads/${project}-paz-profile
+  mkdir /tmp/${project}-paz-profile
 
   printf "\n\033[0;32mGenerate PAZ profile\033[0m\n"
   kubectl exec $paz_pod -- rm -fr /opt/out/instance/profile/
   kubectl exec $paz_pod -- mkdir /opt/out/instance/profile
   kubectl exec $paz_pod -- /opt/out/instance/bin/manage-profile generate-profile --profileRoot /opt/out/instance/profile/
   kubectl exec $paz_pod -- tar cvf /opt/out/instance/paz-profile.tar /opt/out/instance/profile/
-  kubectl cp $mynamespace/$paz_pod:/opt/out/instance/paz-profile.tar ~/Downloads/${project}-paz-profile.tar
+  kubectl cp $K8S_NAMESPACE/$paz_pod:/opt/out/instance/paz-profile.tar /tmp/${project}-paz-profile.tar
 
   printf "\n\033[0;32mExtract files\033[0m\n"
-  tar -xvf ~/Downloads/${project}-paz-profile.tar -C ~/Downloads/${project}-paz-profile
+  tar -xvf /tmp/${project}-paz-profile.tar -C /tmp/${project}-paz-profile
 
   printf "\n\033[0;32mRemove setup-arguments.txt and license file(s)\033[0m\n"
-  rm -fv ~/Downloads/${project}-paz-profile/opt/out/instance/profile/setup-arguments.txt
-  rm -fv ~/Downloads/${project}-paz-profile/opt/out/instance/profile/server-root/pre-setup/*.lic
+  rm -fv /tmp/${project}-paz-profile/opt/out/instance/profile/setup-arguments.txt
+  rm -fv /tmp/${project}-paz-profile/opt/out/instance/profile/server-root/pre-setup/*.lic
 
   # Remove existing profile files from target.
   printf "\n\033[0;32mRemove existing profile files from target.\033[0m\n"
   rm -fr ./profiles/${ping_dir}/pd.profile/
 
   printf "\n\033[0;32mCopy profile\033[0m\n"
-  cp -Rv ~/Downloads/${project}-paz-profile/opt/out/instance/profile/. ./profiles/${ping_dir}/pd.profile/
+  cp -Rv /tmp/${project}-paz-profile/opt/out/instance/profile/. ./profiles/${ping_dir}/pd.profile/
 
   printf "\n\033[0;32mPAZ complete!\033[0m\n"
 }
@@ -214,7 +206,7 @@ pap() {
   else
     paz_snapshot="defaultPolicies.snapshot"
     paz_dir="pingdatagovernancepap"
-    paz_subdomain="pingdatagovernancepap-${mynamespace}"
+    paz_subdomain="pingdatagovernancepap-${K8S_NAMESPACE}"
   fi
 
   # BXRetail snapshot is different than BXFinance
@@ -266,7 +258,7 @@ ping_access() {
     pa_subdomain="pingaccess-admin-${repo}"
     pa_pwd="YWRtaW5pc3RyYXRvcjoyRmVkZXJhdGVNMHJlIQ=="
   else
-    pa_subdomain="pingaccess-${mynamespace}"
+    pa_subdomain="pingaccess-${K8S_NAMESPACE}"
     pa_pwd="YWRtaW5pc3RyYXRvcjoyRmVkZXJhdGVNMHJlIQ=="
   fi
 
@@ -283,54 +275,74 @@ ping_federate() {
   printf "\n\033[0;32mStarting PingFederate Profile Update\033[0m\n"
 
   printf "\n\033[0;32mRemove any previous files or folders\033[0m\n"
-  rm -rf ~/Downloads/${project}-pf-profile
-  rm -f ~/Downloads/${project}-pf-archive.zip
+  rm -rf /tmp/${project}-pf-profile
+  rm -f /tmp/${project}-pf-archive.zip
 
   printf "\n\033[0;32mCreate local folders\033[0m\n"
-  mkdir ~/Downloads/${project}-pf-profile
+  mkdir /tmp/${project}-pf-profile
 
   if [ "$project" = "bxhealth"  ]
   then
     pd_subdomain="pingfederate-admin-${repo}"
   else
-    pd_subdomain="pingfederate-${mynamespace}"
+    pd_subdomain="pingfederate-${K8S_NAMESPACE}"
   fi
 
   # Call PF admin-api to export configuration archive
-  curl -ko ~/Downloads/${project}-pf-archive.zip "https://${pd_subdomain}.ping-devops.com/pf-admin-api/v1/configArchive/export" -H 'accept: application/json' -H 'X-XSRF-Header: PingFederate' -H 'Authorization: Basic YXBpLWFkbWluOjJGZWRlcmF0ZU0wcmU='
+  curl -ko /tmp/${project}-pf-archive.zip "https://${pd_subdomain}.ping-devops.com/pf-admin-api/v1/configArchive/export" -H 'accept: application/json' -H 'X-XSRF-Header: PingFederate' -H 'Authorization: Basic YXBpLWFkbWluOjJGZWRlcmF0ZU0wcmU='
 
   printf "\n\033[0;32mExtract files\033[0m\n"
-  unzip -d ~/Downloads/${project}-pf-profile ~/Downloads/${project}-pf-archive.zip
+  unzip -d /tmp/${project}-pf-profile /tmp/${project}-pf-archive.zip
 
   # Remove existing profile files from target.
   printf "\n\033[0;32mRemove existing profile files from target.\033[0m\n"
   rm -fr ./profiles/pingfederate/instance/server/default/data/
 
   printf "\n\033[0;32mCopy updated profile to local repository\033[0m\n"
-  cp -Rf ~/Downloads/${project}-pf-profile/. ./profiles/pingfederate/instance/server/default/data/
+  cp -Rf /tmp/${project}-pf-profile/. ./profiles/pingfederate/instance/server/default/data/
 
   printf "\n\033[0;32mPingFederate complete!\033[0m\n"
 }
 
-if [ "${choices[0]}" ]
-  then ping_authorize
-fi
+# if [ "${choices[0]}" ]
+#   then ping_authorize
+# fi
 
-if [ "${choices[1]}" ]
-  then pap
-fi
+# if [ "${choices[1]}" ]
+#   then pap
+# fi
 
-if [ "${choices[2]}" ]
-  then ping_directory
-fi
+# if [ "${choices[2]}" ]
+#   then ping_directory
+# fi
 
-if [ "${choices[3]}" ]
-  then ping_access
-fi
+# if [ "${choices[3]}" ]
+#   then ping_access
+# fi
 
-if [ "${choices[4]}" ]
-  then ping_federate
-fi
+# if [ "${choices[4]}" ]
+#   then ping_federate
+# fi
+
+for i in ${!options[@]}; do 
+  echo "i is: $i"
+  echo "options[i] is: ${options[i]}"
+  if test ${choices[i]} = "+"; then
+    echo "choices[i] is: ${choices[i]}"
+    case ${options[i]} in
+      pingaccess-admin)
+        ping_access ;;
+      pingfederate-admin)
+        ping_federate ;;
+      pingdirectory)
+        ping_directory ;;
+      pingauthorize)
+        ping_authorize ;;
+      pingauthorizepap)
+        pap ;;
+    esac
+  fi
+done
 
 # Variablize it
 printf "\n\033[0;32mStarting variablize-profiles\033[0m\n"
